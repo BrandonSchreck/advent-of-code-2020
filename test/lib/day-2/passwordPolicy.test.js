864e776082074767fe2fd7701bda67af68d57a75
bschreck86 <brandonschreck86@gmail.com>
2020-12-02 22:41:42 -0600

    refactored passwordPolicy.js to solve for 2.1, updated readmes and custom badge json

--- test/lib/day-2/passwordPolicy.test.js
+++ test/lib/day-2/passwordPolicy.test.js
@@ -1,5 +1,5 @@
 import assert from 'assert';
-import { validPasswordCount } from '../../../lib/day-2/passwordPolicy.js';
+import { legacyValidPasswordCount, validPasswordCount } from '../../../lib/day-2/passwordPolicy.js';
 
 const relativeFilePath = 'srv/day-2/sample-password-policies.txt';
 
@@ -7,18 +7,18 @@ describe('Password Policy 2.1 - Sample', () => {
     const expectedValue = 2;
 
     it(`should return ${expectedValue} valid password policies`, () => {
-        return validPasswordCount(relativeFilePath, (count) => {
+        return legacyValidPasswordCount(relativeFilePath, (count) => {
             assert.strictEqual(count, expectedValue);
         });
     });
 });
 
-// describe('Password Policy 2.2 - Sample', () => {
-//     const expectedValue = 1;
+describe('Password Policy 2.2 - Sample', () => {
+    const expectedValue = 1;
 
-//     it(`should return ${expectedValue} valid password policies`, () => {
-//         return validPasswordCount(relativeFilePath, (count) => {
-//             assert.strictEqual(1, expectedValue);
-//         });
-//     });
-// });
+    it(`should return ${expectedValue} valid password policies`, () => {
+        return validPasswordCount(relativeFilePath, (count) => {
+            assert.strictEqual(count, expectedValue);
+        });
+    });
+});
