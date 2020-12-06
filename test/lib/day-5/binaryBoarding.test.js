2d6539a85a7d98a01f88bad6acf5d666ee42dd65
bschreck86 <brandonschreck86@gmail.com>
2020-12-06 13:49:31 -0600

    added solution for day 5, refactored code a bit, more changes to come

--- test/lib/day-5/binaryBoarding.test.js
+++ test/lib/day-5/binaryBoarding.test.js
@@ -1,5 +1,13 @@
-// TODO - write tests
-// FBFBBFFRLR - 357
-// BFFFBBFRRR - 567
-// FFFBBBFRRR - 119
-// BBFFBBFRLL - 820
+import assert from 'assert';
+import { solveForOne } from '../../../lib/day-5/binaryBoarding.js';
+
+describe('Password Processor 4.1 test', () => {
+    const relativeFilePath = 'srv/day-5/sample-boarding-passes.txt';
+    const expectedValue = 820;
+
+    it(`should return seatId: ${expectedValue}`, () => {
+        return solveForOne(relativeFilePath, (seatId) => {
+            assert.strictEqual(seatId, expectedValue);
+        });
+    });
+});
