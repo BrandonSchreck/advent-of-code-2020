ca447a9831b023744192bba9ef0a521092a3f283
bschreck86 <brandonschreck86@gmail.com>
2020-12-13 16:01:28 -0600

    added solution for day 10

---
+++ test/lib/day-10/adapterArray.test.js
@@ -0,0 +1,46 @@
+import assert from 'assert';
+import { solveForOne, solveForTwo } from '../../../lib/day-10/adapterArray.js';
+
+describe('Adapter Array 10.1a test', () => {
+    const expectedValue = 35;
+    const relativeFilePath = 'srv/day-10/sample-one-output-joltage.txt';
+
+    it(`${expectedValue} is the difference`, () => {
+        return solveForOne(relativeFilePath, (accumulatorValue) => {
+            assert.strictEqual(accumulatorValue, expectedValue);
+        });
+    });
+});
+
+describe('Adapter Array 10.1b test', () => {
+    const expectedValue = 220;
+    const relativeFilePath = 'srv/day-10/sample-two-output-joltage.txt';
+
+    it(`${expectedValue} is the difference`, () => {
+        return solveForOne(relativeFilePath, (accumulatorValue) => {
+            assert.strictEqual(accumulatorValue, expectedValue);
+        });
+    });
+});
+
+describe('Adapter Array 10.2a test', () => {
+    const expectedValue = 8;
+    const relativeFilePath = 'srv/day-10/sample-one-output-joltage.txt';
+
+    it(`${expectedValue} paths`, () => {
+        return solveForTwo(relativeFilePath, (accumulatorValue) => {
+            assert.strictEqual(accumulatorValue, expectedValue);
+        });
+    });
+});
+
+describe('Adapter Array 10.2b test', () => {
+    const expectedValue = 19208;
+    const relativeFilePath = 'srv/day-10/sample-two-output-joltage.txt';
+
+    it(`${expectedValue} paths`, () => {
+        return solveForTwo(relativeFilePath, (accumulatorValue) => {
+            assert.strictEqual(accumulatorValue, expectedValue);
+        });
+    });
+});
